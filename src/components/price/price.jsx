cc7a630bca583af02fba7f038ccb5e2297a00925
abubakir1111 <shahobiddinovabubakr637@gmail.com>
2024-10-02 14:35:29 +0500

    first commit

--- src/components/price/price.jsx
+++ src/components/price/price.jsx
@@ -1,18 +1,19 @@
-import './price.scss'
-import { footer, price } from './priceApi'
-import footerImg1 from '../../assets/img/footer/Vector1.png'
-import footerImg2 from '../../assets/img/footer/Vector2.png'
-import { Fragment, useRef } from 'react'
+import './price.scss';
+import { footer, price } from './priceApi';
+import footerImg1 from '../../assets/img/footer/Vector1.png';
+import footerImg2 from '../../assets/img/footer/Vector2.png';
+import { Fragment, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import emailjs from '@emailjs/browser';
 
 function Price() {
-
     const swiperRef = useRef(null);
+    const form = useRef();
 
     const handlePrevClick = () => {
         swiperRef.current.swiper.slidePrev();
@@ -21,23 +22,40 @@ function Price() {
     const handleNextClick = () => {
         swiperRef.current.swiper.slideNext();
     };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_7jdl404', 'template_tldzmo5', form.current, 'YOUR_PUBLIC_KEY')
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    form.current.reset();
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                }
+            );
+    };
+
     return (
         <Fragment>
             <div className='price'>
                 <div className="price-box">
-                    <div className="price-card" >
+                    <div className="price-card">
                         <div className="price-text-card">
                             <div className="price-text-card-div1">
                                 <p className="price-text-card-p1">Ипотечные программы</p>
                             </div>
                             <div className="price-text-card-div2">
-                                <p className='price-text-card-p2'>Расчёты являются приблизительными.За точной информацией обращайтесь в офис продаж по тел. +7 812 604-25-68</p>
+                                <p className='price-text-card-p2'>Расчёты являются приблизительными. За точной информацией обращайтесь в офис продаж по тел. +7 812 604-25-68</p>
                             </div>
                         </div>
                         <div className="price-div-programma">
                             <div className="price-div-programma-box">
                                 <div className="price-text1">
-                                    <p>Программа </p>
+                                    <p>Программа</p>
                                 </div>
                                 <div className='price-text-card'>
                                     <p className='price-text'>Ставка</p>
@@ -50,33 +68,33 @@ function Price() {
                             </div>
                         </div>
                         <div className='bank'>
-                            {price.map((price, index) =>
+                            {price.map((item, index) => (
                                 <div className="bank-box" key={index}>
                                     <div className="bank-box-text-card">
                                         <div className="bank-logo">
                                             <div className="bank-text-gap">
                                                 <div className="logo">
-                                                    <img src={price.bankLogo} alt="" />
+                                                    <img src={item.bankLogo} alt="" />
                                                 </div>
                                                 <div className="bank-logo-text">
                                                     <p className='pro1'>Программа</p>
-                                                    <p className='pro2'>{price.bankLogoText}</p>
+                                                    <p className='pro2'>{item.bankLogoText}</p>
                                                 </div>
                                             </div>
                                             <div className="bank-text-center">
                                                 <div className="bank-text-center-div">
                                                     <div className='bank-text-left'>
-                                                        <p>{price.bankLeftP1}</p>
-                                                        <p>{price.bankLeftP2}</p>
-                                                        <p>{price.bankLeftP3}</p>
+                                                        <p>{item.bankLeftP1}</p>
+                                                        <p>{item.bankLeftP2}</p>
+                                                        <p>{item.bankLeftP3}</p>
                                                     </div>
                                                     <div className="bank-left">
-                                                        <p>{price.bankLeftText1}</p>
-                                                        <p>{price.bankLeftText2}</p>
+                                                        <p>{item.bankLeftText1}</p>
+                                                        <p>{item.bankLeftText2}</p>
                                                     </div>
                                                 </div>
                                                 <div className='bank-left-btn'>
-                                                    <button className='bank-left-btn-info'>{price.bankLeftBtn}</button>
+                                                    <button className='bank-left-btn-info'>{item.bankLeftBtn}</button>
                                                 </div>
                                             </div>
                                         </div>
@@ -85,7 +103,7 @@ function Price() {
                                         <div className="div-border"></div>
                                     </div>
                                 </div>
-                            )}
+                            ))}
                             <div className='bank-btn'>
                                 <button>Показать еще</button>
                             </div>
@@ -93,21 +111,23 @@ function Price() {
                     </div>
                 </div>
                 <div className="contact">
-                    <div className="contact-box">
-                        <div className="contact-card">
-                            <div className="contact-box-title">
-                                <p> Мы готовы ответить на все Ваши вопросы</p>
-                            </div>
-                            <div className="btn-card">
-                                <div className="btn-box-card">
-                                    <input type="text" placeholder='Имя' />
-                                    <input type="number" placeholder='+7 --- -- --' />
-                                    <button>Отправить</button>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="contact-box">
+                            <div className="contact-card">
+                                <div className="contact-box-title">
+                                    <p>Мы готовы ответить на все Ваши вопросы</p>
                                 </div>
-                                <p className='btn-card-p'>Нажимая кнопку «Отправить», вы подтверждаете, что согласны на обработку персональных данных.</p>
+                                <div className="btn-card">
+                                    <div className="btn-box-card">
+                                        <input type="text" placeholder='Имя' name='from_name'  />
+                                        <input type="tel" placeholder='+7 --- -- --' name='from_number'  />
+                                        <button type="submit">Отправить</button>
+                                    </div>
+                                    <p className='btn-card-p'>Нажимая кнопку «Отправить», вы подтверждаете, что согласны на обработку персональных данных.</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                     <div className='contact-text'>
                         <p>Отзывы</p>
                     </div>
@@ -120,36 +140,32 @@ function Price() {
                                 grabCursor={true}
                                 centeredSlides={true}
                                 loop={true}
-                                slidesPerView={1.9} // Har bir qatorada uchta kartani ko'rsatish
-                                spaceBetween={0} // Kartalar orasidagi bo'shliq
+                                slidesPerView={1.9}
+                                spaceBetween={0}
                                 coverflowEffect={{
                                     rotate: 0,
                                     stretch: 0,
                                     depth: 100,
-                                    modifier: 1
+                                    modifier: 1,
                                 }}
                                 modules={[EffectCoverflow, Pagination, Navigation]}
                             >
-                                {footer.map((footer) =>
-                                    <SwiperSlide>
-
-                                        <div className="footer-card1" key={footer.id}>
+                                {footer.map((item, index) => (
+                                    <SwiperSlide key={index}>
+                                        <div className="footer-card1">
                                             <div className="footer-text-div">
-                                                <img src={footer.logo_img} alt="" className='footer-img1' />
-                                                <p className='footer-text-p'>{footer.logo_text}в</p>
+                                                <img src={item.logo_img} alt="" className='footer-img1' />
+                                                <p className='footer-text-p'>{item.logo_text}в</p>
                                             </div>
                                             <div className='footer-text-desc'>
-                                                <p>{footer.text1}</p>
+                                                <p>{item.text1}</p>
                                             </div>
                                             <div className='footer-card-text-red'>
-                                                <p>{footer.text2}</p>
+                                                <p>{item.text2}</p>
                                             </div>
                                         </div>
-
-
                                     </SwiperSlide>
-                                )}
-
+                                ))}
                                 <div className="footer-button-div">
                                     <div className="footer-button-box">
                                         <div className="footer-button">
@@ -159,14 +175,12 @@ function Price() {
                                     </div>
                                 </div>
                             </Swiper>
-
                         </div>
-
                     </div>
                 </div>
             </div>
         </Fragment>
-    )
+    );
 }
 
-export default Price
+export default Price;
